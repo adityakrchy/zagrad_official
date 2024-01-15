fc41e9208f45b52646e1d1e4ee1baedfe71e3140
Rahamtullah Sheikh <sheikh856591@gmail.com>
2024-01-15 11:20:02 +0530

    adding login component

--- src/components/auth/LoginEmail.tsx
+++ src/components/auth/LoginEmail.tsx
@@ -9,7 +9,7 @@ const Login = () => {
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-                                Create an account
+                                Sign in to your account
                             </h1>
                             <div className='flex gird grid-cols-2'>
                                 <a href="#" className="mx-1 w-40 flex justify-center items-center border border-black">
